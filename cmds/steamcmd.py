9495017eb4edbc1743ec060c0d67ca518e948693
rave <aj.clark.rave@gmail.com>
2023-10-12 18:50:58 -0700

    feat: all existing queries should now work with async sqlalchemy

--- cmds/steamcmd.py
+++ cmds/steamcmd.py
@@ -1,15 +1,13 @@
 from discord.ext import commands
 from utils.get_steamid_info import get_steamid_info
-from sqlalchemy.orm import sessionmaker
-from sqlalchemy import create_engine, select
+from sqlalchemy.orm import sessionmaker, selectinload
+from sqlalchemy import create_engine, select, delete
 from models.models import SteamidData, DiscordServer, tracking
 from settings import DB_URL
 from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker, AsyncSession
 
 engine = create_async_engine(DB_URL)
-session_maker = async_sessionmaker(engine)
-
-#session_maker = sessionmaker(bind=create_engine(DB_URL))
+session_maker = async_sessionmaker(engine, class_= AsyncSession)
 
 
 @commands.group()
@@ -23,22 +21,30 @@ async def steamid(ctx):
     brief="Add a steamid to track."
 )
 async def add(ctx, steamid: str):
-    with session_maker() as session:
+    async with session_maker() as session:
         serverid = str(ctx.guild.id)
         channelid = str(ctx.channel.id)
 
         # Checking to see if this server is already tracking the software
-        existing_tracking = session.query(SteamidData).\
-            filter(SteamidData.steamid == steamid).\
-            filter(SteamidData.servers.any(DiscordServer.serverid == serverid)).\
-            first()
-        
+        existing_tracking = await session.execute(
+            select(SteamidData)
+            .filter(SteamidData.steamid == steamid)
+            .filter(SteamidData.servers.any(DiscordServer.serverid == serverid))
+            .options(selectinload(SteamidData.servers))
+        )
+        existing_tracking = existing_tracking.scalars().first()
+
         if existing_tracking:
             await ctx.send(f"Already tracking {existing_tracking.name}.")
             return
-        
-        software = session.query(SteamidData).filter_by(steamid=steamid).first()
 
+        # Checking to see if the steamid is already in the database
+        software = await session.execute(
+            select(SteamidData).filter_by(steamid = steamid)
+            .options(selectinload(SteamidData.servers))
+        )
+        software = software.scalar()
+        
         if software is None:
             steamcmd_results = await get_steamid_info(steamid=steamid)
             
@@ -53,10 +59,21 @@ async def add(ctx, steamid: str):
             )
 
             session.add(software)
-            session.commit()
+            await session.commit()
 
-        # Add to tracking table
-        existing_server = session.query(DiscordServer).filter_by(serverid = serverid).first()
+            # We now have to re-query software, since the above commit closes the query
+            software = await session.execute(
+                select(SteamidData).filter_by(steamid = steamid)
+                .options(selectinload(SteamidData.servers))
+            )
+            software = software.scalar()
+
+        # Checking to make sure that this server is already noted within the database
+        existing_server = await session.execute(
+            select(DiscordServer).filter_by(serverid = serverid)
+        )
+        
+        existing_server = existing_server.scalar()
 
         # If a tracking channel isn't set, we're just going to use the channel the command was issued from
         if existing_server is None:
@@ -65,19 +82,18 @@ async def add(ctx, steamid: str):
                 channelid = channelid
             )
 
-            session.add(server)
-            # We need the DiscordServer record  to exist before tracking the software, so we'll create it
-            session.commit()
-
+            session.add(server)        
             software.servers.append(server)
-            session.commit()
+            software_name = software.name
+            await session.commit()
 
-            await ctx.send(f"Added {software.name} to tracked Steam packages.")
+            await ctx.send(f"Added {software_name} to tracked Steam packages.")
             await ctx.send(f"Using {ctx.channel.name} as the notification channel. Run 'set_channel' in the desired channel to change this.")
         else: 
             software.servers.append(existing_server)
-            session.commit()
-            await ctx.send(f"Added {software.name} to tracked Steam packages.")
+            software_name = software.name
+            await session.commit()
+            await ctx.send(f"Added {software_name} to tracked Steam packages.")
 
 
 @steamid.command(
@@ -85,10 +101,12 @@ async def add(ctx, steamid: str):
     brief="Remove a steamid from tracking in DiscordServer for this server."
 )
 async def remove(ctx, steamid: str):
-    with session_maker() as session:
+    async with session_maker() as session:
         serverid = str(ctx.guild.id)
 
-        steamid_data = session.query(SteamidData).filter_by(steamid = steamid).first()
+        steamid_data = await session.execute(select(SteamidData).filter_by(steamid = steamid))
+        steamid_data = steamid_data.scalar()
+        steamid_name = steamid_data.name
 
         if steamid_data is None:
             steamcmd_results = await get_steamid_info(steamid=steamid)
@@ -100,21 +118,21 @@ async def remove(ctx, steamid: str):
             await ctx.send(f"{steamcmd_results['name']} was not being tracked for this server.")
             return
 
-        discord_server = session.query(DiscordServer).filter_by(serverid = serverid).first()
-    
-        remove_tracking = tracking.delete().where(
+        discord_server = await session.execute(select(DiscordServer).filter_by(serverid = serverid))
+        discord_server = discord_server.scalar()
+        
+        remove_tracking = await session.execute(delete(tracking).where(
             (tracking.c.steamid == steamid_data.id) &
             (tracking.c.serverid == discord_server.id)
-        )
+        ))
+        deleted_count = remove_tracking.rowcount
 
-        deleted_count = session.execute(remove_tracking).rowcount
-
-        session.commit()
+        await session.commit()
         
         if deleted_count > 0:
-            await ctx.send(f"Stopped tracking {steamid_data.name} for this server.")
+            await ctx.send(f"Stopped tracking {steamid_name} for this server.")
         else: 
-            await ctx.send(f"{steamid_data.name} was not being tracked for this server.")
+            await ctx.send(f"{steamid_name} was not being tracked for this server.")
 
 
 @steamid.command(
@@ -125,10 +143,14 @@ async def list(ctx):
     async with session_maker() as session:
         serverid = str(ctx.guild.id)
 
-        tracked_software = await session.execute(select(SteamidData).\
-                                                 filter(SteamidData.servers.any(DiscordServer.serverid == serverid)))
+        tracked_software = await session.execute(
+            select(SteamidData).filter(
+                SteamidData.servers.any(DiscordServer.serverid == serverid)
+            )
+        )
+        tracked_software = tracked_software.scalars().all()
 
-        if tracked_software.scalars().all():
+        if tracked_software:
             # Rather than a deluge of messages, we're going to send just one message
             output = ["Name - SteamID"]
             for package in tracked_software:
